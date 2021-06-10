2dff9bdc611c6b614a4823e796fb991d2420b54e
csecec.1703023@gmail.com <csecec.1703023@gmail.com>
2021-06-10 15:30:55 +0530

    Second + Third Commit

---
+++ Data/PlayerDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppGitPlayerEx.Models;
+
+namespace WebAppGitPlayerEx.Data
+{
+    public class PlayerDbContext : DbContext
+    {
+        public PlayerDbContext (DbContextOptions<PlayerDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppGitPlayerEx.Models.Player> Player { get; set; }
+    }
+}
